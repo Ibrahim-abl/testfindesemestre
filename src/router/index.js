1de7c736261674d92e16a3073e25f6579ec85db4
ibrahim-abl <ibrahim.abl@eduge.ch>
2017-11-29 11:41:24 +0100

    soucis index.js à régler, +finir video

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Home from '@/components/Jeux/Jeux'
-import Home from '@/components/Jeux/CreateJeux'
-import Home from '@/components/User/Profile'
-import Home from '@/components/User/Signin'
-import Home from '@/components/User/Signup'
+import Jeux from '@/components/Jeux/Jeux'
+import CreateJeux from '@/components/Jeux/CreateJeux'
+import Profile from '@/components/User/Profile'
+import Signin from '@/components/User/Signin'
+import Signup from '@/components/User/Signup'
 
 Vue.use(Router)
 
@@ -17,29 +17,30 @@ export default new Router({
       component: Home
     },
     {
-      path: '/jeux'
-      name: 'Jeux'
-      components: Jeux
+      path: '/jeux',
+      name: 'Jeux',
+      component: Jeux
     },
     {
-      path: '/jeux/new'
-      name: 'CreateJeux'
-      components: CreateJeux
+      path: '/jeux/new',
+      name: 'CreateJeux',
+      component: CreateJeux
     },
     {
-      path: '/profile'
-      name: 'Profile'
-      components: Profile
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     },
     {
-      path: '/signup'
-      name: 'Signup'
-      components: Signup
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     },
     {
-      path: '/signin'
-      name: 'Signin'
-      components: Signin
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     },
-  ]
+  ],
+  mode: 'history'
 })
